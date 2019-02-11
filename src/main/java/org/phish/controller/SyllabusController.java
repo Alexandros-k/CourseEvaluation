09688029923858642b613bb0dad94136ac3251e1
Alexandros-k <alkon134@gmail.com>
2019-02-11 20:50:32 +0200

    update syllabus controller

--- src/main/java/org/phish/controller/SyllabusController.java
+++ src/main/java/org/phish/controller/SyllabusController.java
@@ -28,7 +28,8 @@ public class SyllabusController {
 	ArrayList<Verb> newVerbNames = new ArrayList<Verb>();
 	ArrayList<Integer>verbscount = new ArrayList<Integer>();
 	 Map <String,Integer> hm = new HashMap<String, Integer>();
-	
+	 Map <String,Integer> hm1 = new HashMap<String, Integer>();
+	 ArrayList<Map <String,Integer>>totalArray = new ArrayList<Map <String,Integer>>();
 	
 	@RequestMapping(value = "/addSyllabus",method = RequestMethod.POST,headers = "Accept=application/json")
 	public  String addSyllabus() throws FileNotFoundException {		
@@ -50,7 +51,7 @@ public class SyllabusController {
 		 String file="C:\\ProfessoruploadSyllabus\\syllabus\\dasa";
 		
 		
-	 List<String> verbNames = fileSearch.parseFile(file, listOfVerbs);
+	 List<String> verbNames = fileSearch.parseFile(filename, listOfVerbs);
 		
 		for (String lucentVerb : verbNames) {
 			for (Verb verb : listOfVerbs) {
@@ -74,8 +75,13 @@ public class SyllabusController {
 	@RequestMapping(value="/statistics",method = RequestMethod.GET,headers = "Accept=application/json")
 	public String getSyllabus(Model model) {
 		
-
-		
+ArrayList<String> AttributeNames = new ArrayList<String>();
+		int remember=0;
+		int understand=0;
+		int apply=0;
+		int analyze=0;
+		int evaluate=0;
+		int create=0;
 		
 		hm.put("remember", 0);
 		hm.put("understand", 0);
@@ -84,37 +90,107 @@ public class SyllabusController {
 		hm.put("evaluate", 0);
 		hm.put("create", 0);
 		
+		AttributeNames.add("remember");
+		AttributeNames.add("understand");
+		AttributeNames.add("apply");
+		AttributeNames.add("analyze");
+		AttributeNames.add("evaluate");
+		AttributeNames.add("create");
+		
+		
 		List<Syllabus> syllabus =  syllabusService.getAllSyllabus();
-	for (Syllabus syllabi : syllabus) {
+
+		
+		Map <Integer,ArrayList<String>> sortedAttr1 = sortAttributes2(syllabus);
+		//Map <String,Integer> sortedAttr=sortAttributes( syllabus );
+		
+	/*	for(Map.Entry<Integer, ArrayList<String>> entry : sortedAttr1.entrySet()) {
+		    Integer attr = entry.getKey();
+		    ArrayList<String> attrValue = entry.getValue();
+		  for (String attrval : attrValue) {
 			
-		List<Verb> verbs =	syllabi.getVerbs();
-		
-		for (Verb verb : verbs) {
-		String attrName =	verb.getAttribute().getName();
-		
-		for(Map.Entry<String, Integer> entry : hm.entrySet()) {
-		    String attr = entry.getKey();
-		    int attrValue = entry.getValue();
-		  
-			if(attrName.equals(attr)) {
-				
-				
-				hm.put(attr, attrValue + 1);
-			}
-		}
-		}
-		}	
-		
-	
+			  for(Map.Entry<String, Integer> entry1 : hm.entrySet()) {
+				    String attr1 = entry1.getKey();
+				    Integer attrValue1 = entry1.getValue();
+				  if(attr1.equals(attrval)) {
+						
+						
+						hm.put(attr1, attrValue1 + 1);
+					}
+				  
+				  
 			
+			  
+			
+		  }
+		}
+		}*/
+		
 		model.addAttribute("SyllabusList",syllabus);
-		model.addAttribute("VerbList",hm);
+		model.addAttribute("VerbList",sortedAttr1);
+		model.addAttribute("VerbCount",hm);
 		return "statistics";
 		
 		
 	}
 
 	
+	
+	private Map <String,Integer> sortAttributes(List<Syllabus> syllabus ){
+		
+		for (Syllabus syllabi : syllabus) {
+			
+			List<Verb> verbs =	syllabi.getVerbs();
+			
+			for (Verb verb : verbs) {
+			String attrName =	verb.getAttribute().getName();
+			
+			for(Map.Entry<String, Integer> entry : hm.entrySet()) {
+			    String attr = entry.getKey();
+			    int attrValue = entry.getValue();
+			  
+				if(attrName.equals(attr)) {
+					
+					
+					hm.put(attr, attrValue + 1);
+				}
+			}
+			}
+			}	
+		
+		return hm;
+				
+	}
+	
+	
+	private Map <Integer,ArrayList<String>>  sortAttributes2(List<Syllabus> syllabus ){
+		Integer counter=0;
+		ArrayList<String> attributeCounter = new ArrayList<String>();
+		//ArrayList<String> attributecollection = new ArrayList<String>();
+		 Map <Integer,ArrayList<String>> attributecollection = new HashMap<Integer, ArrayList<String>>();
+		for (Syllabus syllabi : syllabus) {
+			
+			List<Verb> verbs =	syllabi.getVerbs();
+			
+			for (Verb verb : verbs) {
+			String attrName =	verb.getAttribute().getName();
+		
+			attributeCounter.add(attrName);
+		
+			}
+			
+			
+			
+			attributecollection.put(counter,new ArrayList<String>(attributeCounter));
+			
+			attributeCounter.clear();
+			
+			counter++;
+			}	
+		
+		return attributecollection;
+				
+	}
 	
 	
 	
