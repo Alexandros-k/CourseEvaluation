946fc6c920180f34ebcd3440a70739ac118f0d35
Alexandros-k <alkon134@gmail.com>
2019-02-16 17:50:55 +0200

    sdfsd

--- src/main/java/org/phish/model/UserDetails.java
+++ src/main/java/org/phish/model/UserDetails.java
@@ -22,8 +22,7 @@ public class UserDetails implements  Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")	
 	private Integer id;	
 
 	@Column(name="firstname")
@@ -32,7 +31,7 @@ public class UserDetails implements  Serializable {
 	@Column(name="lastname")
 	private String lastName;
 	
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="id")
 	private User user;
 	 
