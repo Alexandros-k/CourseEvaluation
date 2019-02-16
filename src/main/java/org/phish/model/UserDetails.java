33be2b9fcaf819a744cfd3bdd659b8074bdd0bc6
Alexandros-k <alkon134@gmail.com>
2019-02-16 19:06:18 +0200

    ddsfds

--- src/main/java/org/phish/model/UserDetails.java
+++ src/main/java/org/phish/model/UserDetails.java
@@ -32,7 +32,7 @@ public class UserDetails implements  Serializable {
 	@Column(name="lastname")
 	private String lastName;
 	
-	@OneToOne
+	@OneToOne(cascade = { CascadeType.ALL },fetch = FetchType.EAGER)
 	@JoinColumn(name="id")
 	private User user;
 	 
