b4c1964c57aa00c849b4db4020ddc85f48a37755
Alexandros-k <alkon134@gmail.com>
2019-02-16 18:57:58 +0200

    adsad

--- src/main/java/org/phish/model/User.java
+++ src/main/java/org/phish/model/User.java
@@ -26,7 +26,7 @@ public class User implements  Serializable{
 
 	@Id
 	@Column(name="id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@Column(name="username")
