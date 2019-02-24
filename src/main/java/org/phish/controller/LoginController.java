b798e92dee59b618608d00ae02aed33fcaa6fbb0
Alexandros-k <alkon134@gmail.com>
2019-02-24 14:25:17 +0200

    login controller fix

--- src/main/java/org/phish/controller/LoginController.java
+++ src/main/java/org/phish/controller/LoginController.java
@@ -2,4 +2,16 @@ package org.phish.controller;
 
 public class LoginController {
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("")
+public class LoginController {
+	@RequestMapping(method = RequestMethod.GET, value = "/custom_login")
+	public String showLogin() {
+		return "login";
+	}
 }
